5ae26bf68dec3056a75f510fbf193db396e29052
Marek Balvín <marek267balvin@gmail.com>
2023-10-26 17:54:38 +0200

    Initialized auth0 + testing components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import LogInButton from "./components/LogInButton";
+import LogOutButton from "./components/LogOutButton";
+import Profile from "./components/Profile";
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+  if (isLoading) return <div>Loading</div>;
   return (
     <div>
       <h1>App</h1>
+      {!isAuthenticated ? <LogInButton /> : <LogOutButton />}
+      <Profile />
     </div>
   );
 }
