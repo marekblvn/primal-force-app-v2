af55f6bdfd9b31178080f6731ca8375e9751dcd7
Marek Balvín <marek267balvin@gmail.com>
2023-10-26 18:02:26 +0200

    Added lsi context

---
+++ src/contexts/lsi/lsi-context.js
@@ -0,0 +1,3 @@
+import { createContext } from "react";
+const LsiContext = createContext();
+export default LsiContext;
---
+++ src/contexts/lsi/lsi-provider.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import LsiContext from "./lsi-context";
+
+const LsiProvider = ({ children }) => {
+  const [language, setLanguage] = useState(
+    localStorage.getItem("user_pref_language")
+  );
+  useEffect(() => {
+    if (!language) {
+      setLanguage("en");
+      localStorage.setItem("user_pref_language", "en");
+    }
+  }, [language]);
+
+  const handleChangeLanguage = (lang) => {
+    setLanguage(lang);
+    localStorage.setItem("user_pref_language", lang);
+  };
+  return (
+    <LsiContext.Provider value={{ language, handleChangeLanguage }}>
+      {children}
+    </LsiContext.Provider>
+  );
+};
+
+export default LsiProvider;
