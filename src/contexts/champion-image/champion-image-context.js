0d4091cd5d901edd58b141fed983d429c41b686d
Marek Balvín <marek267balvin@gmail.com>
2023-10-26 14:01:03 +0200

    Added Champion Image context

---
+++ src/contexts/champion-image/champion-image-context.js
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+
+const ChampionImageContext = createContext();
+export default ChampionImageContext;
---
+++ src/contexts/champion-image/champion-image-provider.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import ChampionImageContext from "./champion-image-context";
+import champions from "../../static/data/champions.json";
+import { baseCdnUrl, lolPatch } from "../../static/data/config.json";
+
+const ChampionImageProvider = ({ children }) => {
+  const [championImages, setChampionImages] = useState([]);
+  useEffect(() => {
+    const loadImages = async () => {
+      const images = await Promise.all(
+        champions.map(
+          (champion) =>
+            new Promise((resolve) => {
+              const img = new Image();
+              img.src =
+                baseCdnUrl + lolPatch + "/img/champion/" + champion.asset;
+              img.onload = () => resolve(img);
+            })
+        )
+      );
+      const imagesMap = images.map((img, index) => [
+        champions[index].asset.split(".")[0],
+        img,
+      ]); //FIXME: This surely can be achieved easier
+      setChampionImages(Object.fromEntries(imagesMap));
+    };
+    loadImages();
+  }, []);
+  return (
+    <ChampionImageContext.Provider value={championImages}>
+      {children}
+    </ChampionImageContext.Provider>
+  );
+};
+export default ChampionImageProvider;
