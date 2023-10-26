e5b8d1721c230f063f2607956e25cc79cbd87968
Marek Balvín <marek267balvin@gmail.com>
2023-10-26 13:30:52 +0200

    Added summoner spells

---
+++ src/utils/summoner-spell-map.js
@@ -0,0 +1,25 @@
+import flash from "../static/img/summoner-spells/flash.png";
+import heal from "../static/img/summoner-spells/heal.png";
+import exhaust from "../static/img/summoner-spells/exhaust.png";
+import clarity from "../static/img/summoner-spells/clarity.png";
+import teleport from "../static/img/summoner-spells/teleport.png";
+import ghost from "../static/img/summoner-spells/ghost.png";
+import cleanse from "../static/img/summoner-spells/cleanse.png";
+import barrier from "../static/img/summoner-spells/barrier.png";
+import ignite from "../static/img/summoner-spells/ignite.png";
+import smite from "../static/img/summoner-spells/smite.png";
+
+const map = {
+  1: cleanse,
+  3: exhaust,
+  4: flash,
+  6: ghost,
+  7: heal,
+  11: smite,
+  12: teleport,
+  13: clarity,
+  14: ignite,
+  21: barrier,
+};
+
+export default map;
