06c8bc4fc2892d76382e00ea4ccaffe70b2462a4
Marek Balvín <marek267balvin@gmail.com>
2023-10-27 10:58:24 +0200

    Added ThemeProvider

--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./utils/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: process.env.REACT_APP_AUTH0_API,
-        scope: "openid profile email read:match delete:match create:match",
-      }}
-    >
-      <App />
-    </Auth0Provider>
+    <ThemeProvider theme={theme}>
+      <Auth0Provider
+        domain={process.env.REACT_APP_AUTH0_DOMAIN}
+        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: process.env.REACT_APP_AUTH0_API,
+          scope: "openid profile email read:match delete:match create:match",
+        }}
+      >
+        <App />
+      </Auth0Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
