04dd41cdbd0d609761d58e8aa5418e470cde1a4a
Marek Balvín <marek267balvin@gmail.com>
2023-10-28 12:56:19 +0200

    Changes to manifest and index.html

--- src/index.js
+++ src/index.js
@@ -10,26 +10,23 @@ import theme from "./utils/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <ChampionImageProvider>
-        <LsiProvider>
-          <Auth0Provider
-            domain={process.env.REACT_APP_AUTH0_DOMAIN}
-            clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-            authorizationParams={{
-              redirect_uri: window.location.origin,
-              audience: process.env.REACT_APP_AUTH0_API,
-              scope:
-                "openid profile email read:match delete:match create:match",
-            }}
-          >
-            <App />
-          </Auth0Provider>
-        </LsiProvider>
-      </ChampionImageProvider>
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <ChampionImageProvider>
+      <LsiProvider>
+        <Auth0Provider
+          domain={process.env.REACT_APP_AUTH0_DOMAIN}
+          clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+          authorizationParams={{
+            redirect_uri: window.location.origin,
+            audience: process.env.REACT_APP_AUTH0_API,
+            scope: "openid profile email read:match delete:match create:match",
+          }}
+        >
+          <App />
+        </Auth0Provider>
+      </LsiProvider>
+    </ChampionImageProvider>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
