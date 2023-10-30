da2b3b5254e10526a9137aa125dbbc229bc0a318
Marek Balvín <marek267balvin@gmail.com>
2023-10-30 20:48:06 +0100

    Implemented useGetCommand hook

---
+++ src/hooks/useGetCommand.js
@@ -0,0 +1,34 @@
+const { useState, useEffect } = require("react");
+
+const useGetCommand = (command, { skip = false, initialParams = {} }) => {
+  const [data, setData] = useState(undefined);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (error) return;
+    if (!skip) {
+      get(initialParams);
+    }
+  }, []);
+
+  const resetError = () => setError(null);
+
+  const get = async (params = {}) => {
+    resetError();
+    setLoading(true);
+    const res = await command(params);
+    setLoading(false);
+    if (!res.ok) {
+      return setError(res.data?.error);
+    }
+    setData(res.data);
+  };
+
+  return {
+    data,
+    error,
+    loading,
+    get,
+  };
+};
