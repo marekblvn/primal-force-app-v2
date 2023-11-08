c5db592d86b811653c16a095766ac05629792e70
Marek Balvín <marek267balvin@gmail.com>
2023-11-08 14:29:47 +0100

    Hopefully fixed searchbar responsivity

--- src/components/SearchBar/ActionButton.jsx
+++ src/components/SearchBar/ActionButton.jsx
@@ -1,14 +1,16 @@
 import { IconButton, useTheme } from "@mui/material";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 const ActionButton = ({ icon, borderRadius, onClick, hidden }) => {
+  const { width } = useWindowDimensions();
   const theme = useTheme();
   return (
     <IconButton
       sx={{
         visibility: hidden ? "hidden" : "visible",
         backgroundColor: theme.palette.white.main,
-        width: { xs: "31px", sm: "44px" },
-        height: { xs: "31px", sm: "44px" },
+        width: { xs: width > 400 ? "31px" : "24px", sm: "44px" },
+        height: { xs: width > 400 ? "31px" : "24px", sm: "44px" },
         margin: 0,
         borderRadius: borderRadius,
         color: theme.palette.primary.main,
