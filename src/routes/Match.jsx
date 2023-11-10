275d94fe0b56d1292ad1d8da3cd66d2b2135c915
Marek Balvín <marek267balvin@gmail.com>
2023-11-11 00:14:33 +0100

    /match/:id now redirects to MatchDetail route

--- src/routes/Match.jsx
+++ src/routes/Match.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import HorizontalBar from "../components/HorizontalBar";
+import useGetCommand from "../hooks/useGetCommand";
+import { matchGet } from "../services/primal-force-api/match-service";
 
 const Match = () => {
-  return <div>Match</div>;
+  const { matchId } = useParams();
+  const { data, loading, error } = useGetCommand({
+    command: matchGet,
+    skipInitial: false,
+    initialParams: { id: matchId },
+  });
+  return (
+    <>
+      <HorizontalBar />
+      {/* MatchDetailProvider */}
+    </>
+  );
 };
 
 export default Match;
